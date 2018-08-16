1c3c694bfb9e764131324b2431f28a216af4cdfe
Ean Wong <eanwong6@gmail.com>
2018-08-16 10:54:14 -0400

    Added tkam, updated txt_to_json overview

--- txt_to_json.py
+++ txt_to_json.py
@@ -1,6 +1,6 @@
 #Goal: convert regular text into more readable json for WOz prototype
 
-# Use: python3 txt_to_json.py 
+# Use: python3 txt_to_json.py input_file.txt output_file.json
 
 #GIVEN
 #Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
